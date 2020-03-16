44bf24c8da593f9f1735296e0c44c4104905d236
Krishna Ammineni <krishna.ammineni@gmail.com>
2020-03-16 13:17:10 +0530

    key

--- Master-node/terraform/instance.tf
+++ Master-node/terraform/instance.tf
@@ -36,7 +36,8 @@ resource "google_compute_instance" "master" {
   metadata = {
     name = "master"
   #  user-data = "${file("${path.module}/cloud-init.yaml")}"
-    ssh-keys = "${var.ssh_user}:${var.public_ssh_key}"
+    #ssh-keys = "${var.ssh_user}:${var.public_ssh_key}"
+    ssh-keys = "${var.ssh_user}:${file("public_ssh_key")}"
   }
 
   #metadata_startup_script = "echo hi > /test.txt"
@@ -93,19 +94,6 @@ count = length(var.worker_name)
     #ssh-keys = "${var.ssh_user}:${file(var.public_ssh_key)}"
     ssh-keys = "${var.ssh_user}:"${file("public_ssh_key")}"
   }
-
-  #metadata_startup_script = "${file("deploy.sh")}"
-
-  /*provisioner "remote-exec" {
-    script = var.script_path
-    connection {
-      type        = "ssh"
-      host        = google_compute_instance.automation-node.network_interface.0.access_config.0.nat_ip
-      user        = var.ssh_user
-      private_key = file(var.private_ssh_key)
-    }
-  } */
-
 
   service_account {
     scopes = ["userinfo-email", "compute-ro", "storage-ro"]
